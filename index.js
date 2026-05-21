3a74d9f47ace8444445d502fd25493994f171c21
Jayed-AJ <jayedahmed925@gmail.com>
2026-05-21 11:19:20 +0600

    fixIssue

--- index.js
+++ index.js
@@ -136,6 +136,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+}
+
+module.exports = app;
