0ee9f6e93c8029d79e201b9f1041c8869513cb24
Jayed-AJ <jayedahmed925@gmail.com>
2026-05-23 22:07:34 +0600

    Mongodb is now using cached connection

--- api/index.js
+++ api/index.js
@@ -1,29 +1,19 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion, Collection, ObjectId } = require('mongodb');
+const clientPromise = require('../lib/mongodb');
 const app = express();
-// const port =process.env.PORT || 2000;
 
 
 //middle ware
 app.use(cors())
 app.use(express.json())
 
-const uri = `mongodb://${process.env.DB_User}:${process.env.DB_Pass}@ac-v3pf2i9-shard-00-00.aic4ezy.mongodb.net:27017,ac-v3pf2i9-shard-00-01.aic4ezy.mongodb.net:27017,ac-v3pf2i9-shard-00-02.aic4ezy.mongodb.net:27017/?ssl=true&replicaSet=atlas-s0bfev-shard-0&authSource=admin&appName=Cluster1`;
+const client = clientPromise;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+
     await client.connect();
 
   // const database = client.db('coffessDB');
@@ -156,6 +146,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// const port =process.env.PORT || 2000;
 // app.listen(port, () => {
 //   console.log(`Server running on port ${port}`);
 // });
